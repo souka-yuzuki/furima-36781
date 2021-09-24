bde8b13ee80d5a54ecf8c6c66196675e5e3d705d
souka <blackbeanmame@msn.com>
2021-09-24 17:35:54 +0900

    必要なカラムを加筆

--- db/migrate/20210924081155_devise_create_users.rb
+++ db/migrate/20210924081155_devise_create_users.rb
@@ -6,7 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :niclname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_reading, null: false
+      t.string :last_name_reading,  null: false
+      t.Date :brithday,             null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
