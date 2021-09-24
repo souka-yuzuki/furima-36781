1d290791e293c348ba728316155e3216c4ac4e2c
souka <blackbeanmame@msn.com>
2021-09-24 21:04:57 +0900

    deviseにストロングパラメーターで保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, key: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :brithday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
