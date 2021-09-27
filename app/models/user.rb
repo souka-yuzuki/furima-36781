ec298c0c7c840e28c0079e236970a45ffd25d73f
souka <blackbeanmame@msn.com>
2021-09-27 16:13:46 +0900

    名前のバリテーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
-         validates :first_name,         presence: true
-         validates :last_name,          presence: true
-         validates :first_name_reading, presence: true
-         validates :last_name_reading,  presence: true
          validates :birthday,           presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_reading
+    validates :last_name_reading
+  end
 end
