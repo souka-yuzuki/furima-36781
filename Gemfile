a7524f757e567bc175d93a95a42c48530512da98
souka <blackbeanmame@msn.com>
2021-09-27 17:50:38 +0900

    pry導入

--- Gemfile
+++ Gemfile
@@ -61,3 +61,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'aws-sdk-s3', require: false
 
 gem 'devise'
+gem 'pry-rails'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,16 @@
 FactoryBot.define do
   factory :user do
-    Faker::Config.locale = :ja
+    transient do
+      person { Gimei.name }
+    end
     nickname                {Faker::Name.name}
     email                   {Faker::Internet.free_mail}
     password                {Faker::Internet.password(min_length: 6)}
     password_confirmation   {password}
-    first_name              {Faker::Name.name}
-    last_name               {Faker::Name.name}
-    first_name_reading      {Faker::Name.}
-    last_name_reading       {Faker::Name.}
-    birthday                {Faker::Date.}
+    first_name              {person.first.kanji}
+    last_name               {person.last.kanji}
+    first_name_reading      {person.first.katakana}
+    last_name_reading       {person.last.katakana}
+    birthday                {Faker::Date.backward}
   end
 end
