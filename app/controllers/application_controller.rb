55fc143f1f0de845c4ccd78c751942589d341d0f
souka <blackbeanmame@msn.com>
2021-09-27 15:38:45 +0900

    ログイン、サインインできるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, key: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :brithday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
   end
 
   def basic_auth
--- db/migrate/20210924081155_devise_create_users.rb
+++ db/migrate/20210924081155_devise_create_users.rb
@@ -6,12 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :niclname,           null: false
+      t.string :nickname,           null: false
       t.string :first_name,         null: false
       t.string :last_name,          null: false
       t.string :first_name_reading, null: false
       t.string :last_name_reading,  null: false
-      t.date :brithday,         null: false
+      t.date :birthday,         null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
