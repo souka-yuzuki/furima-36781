ad96e4894252d6b58036870a6e80e9a86eb8180c
souka <blackbeanmame@msn.com>
2021-09-27 16:37:22 +0900

    FactoryBotとFaker導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context 'ユーザー情報が正しければ登録できる' do
+      
+    end
+    context 'ユーザー情報に誤りがあると登録できない' do
+      
+    end
+  end
+
 end
