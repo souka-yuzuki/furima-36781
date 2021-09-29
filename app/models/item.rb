9929400ec1560cf05cc509da0f6335a4f53da45c
souka <blackbeanmame@msn.com>
2021-09-29 14:27:53 +0900

    名称変更Load→Status

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   validates :title, :explanation, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :load_id, numericality: { other_than: 1, message: "can't be blank"}
 
 end
