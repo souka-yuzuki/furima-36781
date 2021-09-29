4c408e14d00e36bdeff8820ccc9c5fab10f92672
souka <blackbeanmame@msn.com>
2021-09-29 14:50:35 +0900

    PrefectureのActiveHashを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
   belongs_to :load
+  belongs_to :prefecture
 
   validates :title, :explanation, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :load_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
 
 end
