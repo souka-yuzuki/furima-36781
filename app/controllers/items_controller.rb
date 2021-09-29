7eec87dab9eef3b6e0a8e4d3ea07928188702992
souka <blackbeanmame@msn.com>
2021-09-29 12:22:18 +0900

    カテゴリのActiveHashを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :explanation, :category_id, :status_id, :load_id, :prefecture_id, :shipping_days_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :title, :explanation, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
 end
