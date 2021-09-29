3ae13037a72e7983adbaeef6d5a03affeed83c6d
souka <blackbeanmame@msn.com>
2021-09-29 12:36:20 +0900

    LoadのActiveHashを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :load
 
   validates :title, :explanation, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :load_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
