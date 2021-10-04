146d56c2ebf492e524a31b6bba279cec31e40d92
souka <blackbeanmame@msn.com>
2021-10-04 18:17:38 +0900

    後の実装用にコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :item
-  has_many :purchases
+  #has_many :purchases
 
          validates :nickname,           presence: true
          validates :birthday,           presence: true
