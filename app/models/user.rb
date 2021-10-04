f94115f93b068555e4fa0194b0780872c13d9ce4
souka <blackbeanmame@msn.com>
2021-10-04 16:16:21 +0900

    アソシエーションを定義する

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :item
+  has_many :purchases
 
          validates :nickname,           presence: true
          validates :birthday,           presence: true
