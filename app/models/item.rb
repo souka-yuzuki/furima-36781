4070fd30d3cea36717a709e3ab294433d12d80b6
souka <blackbeanmame@msn.com>
2021-10-05 14:56:50 +0900

    コメントアウト調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_days
   belongs_to :user
   has_one_attached :image
-  has_one :purchase
+  #has_one :purchase
 
   validates :title,             presence: true
   validates :explanation,       presence: true
